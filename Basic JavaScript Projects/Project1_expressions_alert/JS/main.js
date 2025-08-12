d37e22f230170cb6fe45d2618f90a1e27a83e51e
mdtite <marius.tite@gmail.com>
2025-08-12 15:17:59 +0100

    variables

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,10 @@
-alert("hello world!")
+ document.write("Hello World! \"This is a quote\""  + " string.  ")
+ window.alert("This is an alert message")
+
+var A = "This is a string var "
+var B = "This is a second alert"
+document.write(A)
+alert(B)
+
+var family = "AllOfUs", dad = "Peter", mom = "Ana", me ="Marius"
+document.write(dad)
