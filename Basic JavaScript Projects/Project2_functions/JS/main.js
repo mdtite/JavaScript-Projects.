79a0d59396759669d9267557c5e82ed4843008ab
mdtite <marius.tite@gmail.com>
2025-08-12 15:45:38 +0100

    project 2

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction () {
+    var sentance = "I am learning" //creating var
+    sentance += " how to code!" //using the += operator to add to the var
+    document.getElementById("concatenate").innerHTML = sentance //usung get element by id to change the text
+}
