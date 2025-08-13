d5db4c780e97c31bbc7c14ca9c1830511db693d7
mdtite <marius.tite@gmail.com>
2025-08-13 13:13:27 +0100

    project 3 addition

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,4 @@
+function addition_Function () {
+    var addition = 2+2; //creating var
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition //usung get element by id to change the text
+}
