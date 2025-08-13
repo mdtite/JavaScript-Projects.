3ed9aae1481c5b1da6d6c2a0eb6a121c38264bf7
mdtite <marius.tite@gmail.com>
2025-08-13 13:43:52 +0100

    all the operatiors

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,19 @@
 function addition_Function () {
     var addition = 2+2; //creating var
-    document.getElementById("Math").innerHTML = "2 + 2 = " + addition //usung get element by id to change the text
+    document.getElementById("Math").innerHTML = "Addition 2 + 2 = " + addition //usung get element by id to change the text
+}
+
+function subtraction_Function () {
+    var subtraction = 5-2; //creating var
+    document.getElementById("Math2").innerHTML = "Subtraction 5 + 2 = " + subtraction //usung get element by id to change the text
+}
+
+function mult_Function () {
+    var mult = 6*8; //creating var
+    document.getElementById("Math3").innerHTML = "Subtraction 6 * 8 = " + mult //usung get element by id to change the text
+}
+
+function division_Function () {
+    var division = 48/6; //creating var
+    document.getElementById("Math4").innerHTML = "Division 48 / 6 = " + division //usung get element by id to change the text
 }
