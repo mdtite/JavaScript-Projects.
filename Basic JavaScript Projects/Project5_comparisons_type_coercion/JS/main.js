2c0011d3fcd69679c264df91fdaa303d7e1a8c6f
mdtite <marius.tite@gmail.com>
2025-08-19 13:09:51 +0100

    infinity

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,11 +1,14 @@
-document.write(typeof "word");
+document.write(typeof "word"); //string
 
-document.write(typeof 3);
+document.write(typeof 3); //number
 
-document.write("10" + 5);
+document.write("10" + 5); //result is 105 as ads string and the number
 
 function my_function() {
-    document.getElementById("test").innerHTML = 0/0;
-    document.getElementById("test2").innerHTML = isNaN("this is a string");
-    document.getElementById("test3").innerHTML = isNaN("007");
+    document.getElementById("test").innerHTML = 0/0; //NaN
+    document.getElementById("test2").innerHTML = isNaN("this is a string"); //true
+    document.getElementById("test3").innerHTML = isNaN("007"); //false
 }
+
+document.write(2E310); //infinity
+document.write(-2E310); // minus infinity
