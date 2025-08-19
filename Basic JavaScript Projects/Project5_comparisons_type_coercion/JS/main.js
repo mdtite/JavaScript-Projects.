5d3653baa11b9ac58331f1616bc0f117733d519b
mdtite <marius.tite@gmail.com>
2025-08-19 13:07:21 +0100

    nan true false

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -3,3 +3,9 @@ document.write(typeof "word");
 document.write(typeof 3);
 
 document.write("10" + 5);
+
+function my_function() {
+    document.getElementById("test").innerHTML = 0/0;
+    document.getElementById("test2").innerHTML = isNaN("this is a string");
+    document.getElementById("test3").innerHTML = isNaN("007");
+}
