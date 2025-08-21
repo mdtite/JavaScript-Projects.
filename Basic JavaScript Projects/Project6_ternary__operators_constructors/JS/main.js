8c91f94268583782d4a6cb644e5a8b6b7f5cc9f0
mdtite <marius.tite@gmail.com>
2025-08-21 12:39:08 +0100

    constructor

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -11,3 +11,19 @@ function age_function() {
     can_vote = (age <= 18) ? "you are too young":"you are old enough";
     document.getElementById("agev").innerHTML = can_vote + " to vote!";
 }
+
+function vehicle(make, model, year, color) {
+    this.vehicle_make = make;
+    this.vehicle_model = model;
+    this.vehicle_year = year;
+    this.vehicle_color = color;
+}
+
+var Jack = new vehicle("dodge", "viper", "2020", "red")
+var Emily = new vehicle("jeep", "hawk", "2019", "black")
+var Mark = new vehicle("ford", "focus", "1971", "gold")
+
+function my_function() {
+    document.getElementById("keywords_and_constructors").innerHTML = 
+    "Mark drives a " + Mark.vehicle_color + "-colored " + Mark.vehicle_model + " manufactured in " + Mark.vehicle_year;
+}
