a33c50a82684b55fe92b8ea821e5d5caa72adde8
mdtite <marius.tite@gmail.com>
2025-08-30 14:15:26 +0100

    slice uppercase and search

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -7,3 +7,15 @@ function full() {
     var x = a.concat(b,c);
     document.getElementById("conca").innerHTML = x;
 }
+
+// slice and to upper case and search
+
+function sliceF() {
+    var sentance = "All work and no play makes Johnny a dull student."
+    var section = sentance.slice(27,33);
+    var bigLetter = sentance.toUpperCase();
+    var position = sentance.search("work");
+    document.getElementById("slice").innerHTML = section;
+    document.getElementById("upperC").innerHTML = bigLetter;
+    document.getElementById("poss").innerHTML = position;
+}
