99a44fd0ab9979367e5dd3bba4fc483b1b53825a
mdtite <marius.tite@gmail.com>
2025-08-30 14:04:04 +0100

    concat

---
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,9 @@
+// concat
+
+function full() {
+    var a = "i have ";
+    var b = "two ";
+    var c = "apples.";
+    var x = a.concat(b,c);
+    document.getElementById("conca").innerHTML = x;
+}
