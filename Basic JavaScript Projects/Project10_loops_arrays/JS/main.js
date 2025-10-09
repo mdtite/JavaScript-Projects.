22ebadfc3711c180f4b9af596969c37f5aace730
mdtite <marius.tite@gmail.com>
2025-10-09 13:04:17 +0100

    while loop and txt length

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,15 @@
+function Call_Loop() {
+    var a ="";
+    var b = 1;
+while (b < 4) {
+  a += "<br>" + b;
+  b++;
+}
+var text = "Hello World!";
+var length = text.length;
+document.getElementById("Loop").innerHTML = a + "<br>" + length;
+}
+
+var text = "Hello World!";
+var length = text.length;
+
