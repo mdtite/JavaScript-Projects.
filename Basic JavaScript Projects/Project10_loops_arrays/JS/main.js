0061b751adb0e4a58fc36a89f34c885ae0c19f5e
mdtite <marius.tite@gmail.com>
2025-10-09 13:40:02 +0100

    for loop

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -13,3 +13,12 @@ document.getElementById("Loop").innerHTML = a + "<br>" + length;
 var text = "Hello World!";
 var length = text.length;
 
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop () {
+for (Y = 0; Y < Instruments.length; Y++) {
+Content += Instruments[Y] + "<br>";
+}
+document.getElementById("List_of_Instruments"). innerHTML = Content;
+}
